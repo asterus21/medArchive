69a1abc1be4040161644191587c5151fa6bc75b3
Mikhail Palvov <mpavlov@megaputer.ru>
2025-05-06 15:45:26 +0300

    Some comments add

--- script.py
+++ script.py
@@ -16,7 +16,10 @@ class Parse():
 
     def find_element(self, point: str) -> None:
 
-        """The function accepts an element of a website to click."""
+        """
+        The function accepts an element of a website to click, e.g. a button or a drop-down menu. 
+        Such an element is found by its XPath. Whether there are any errors, an exception will be raised.
+        """
 
         try:
             driver.find_element(By.XPATH, point).click()
@@ -27,7 +30,10 @@ class Parse():
 
     def parse_page(self, regex: str, html: str) -> list:
 
-        """The function accepts a raw string to parse and returns a list of parsed elements."""
+        """
+        The function accepts a raw string to parse and returns a list of parsed elements. 
+        Such a string can contain a regular expression as well.
+        """
 
         items = re.findall(regex, html)
         return items
@@ -62,23 +68,27 @@ class Parse():
 
 if __name__ == "__main__":
 
+    '''Here we're creating all used classes instances to operate them afterwards.'''
     constants = Constants()
     options = Options()
     parse = Parse()
     service = Service()
 
+    '''We are using the '--headless' option not to open the browser window.'''
     options.add_argument('--headless')
 
+    '''We are calling the browser with the option in hand and its driver as well.'''
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(constants.constants_to_store(1))
 
+    '''We must be sure of the website we are going to parse.'''
     TITLE = "Архив клинических рекомендаций"
     assert TITLE in driver.title
 
-    '''Here we open the drop-down menu of the articles list.'''
+    '''Here we are opening the drop-down menu of the articles list.'''
     parse.find_element(constants.constants_to_store(2))
 
-    '''Here we choose the 'Все' value.'''
+    '''And choosing the 'Все' value to see a full list of articles.'''
     parse.find_element(constants.constants_to_store(3))
 
     '''Here we are waiting just in case the page is not loaded completely.'''
@@ -90,19 +100,26 @@ if __name__ == "__main__":
         driver.quit()
         sys.exit(1)
 
+    '''We must get a raw HTML page first.'''
     html_text = driver.page_source
 
+    '''This HTML page contains the URLs of the articles and some indices used as their names.'''
     indices = parse.parse_page(constants.constants_to_store(5), html_text)
     url = parse.parse_page(constants.constants_to_store(6), html_text)
 
+    '''We must change the indices of the articles and their URLs due to the files naming restrictions on Windows 10.'''
     indices_revised, url_revised = parse.add_character(indices, url)
     
+    '''And replace some symbols in the articles titles themselves.'''
     titles_revised = parse.replace_character(parse.parse_page(constants.constants_to_store(7), html_text))
 
+    '''We must be sure that we are going to download the same amount of articles as the number of indices and titles.'''
     assert len(url_revised) == len(indices_revised) == len(titles_revised)
 
+    '''Finally, we have a list of article names to use.'''
     results = [prefix + name for prefix, name in zip(indices_revised, titles_revised)]
 
+    '''The rest is to send a GET request to each URL to save an article under a name from the list.'''
     for link, result in zip(url_revised, results):
         r = requests.get(link)
         if r.status_code == 200:
