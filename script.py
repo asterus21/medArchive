0474287f2229fc470d3239b297dc7f1b4ffea7f0
Mikhail Palvov <mpavlov@megaputer.ru>
2025-08-06 12:17:14 +0300

    Main function added

--- script.py
+++ script.py
@@ -10,30 +10,32 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 
+from data import Paths, Regulars
+
 '''The constants to use in the script.'''
-# DRIVER = 'driver/chromedriver.exe' # note that the relative path to the driver is used
+
 LINK = 'https://cr.minzdrav.gov.ru/archive'
-START = '//*[@id="app"]/div/div/main/div/div/div[3]/div[2]/div[1]/div/div/div/div[4]/i'
-END = '//*[@id="v-menu-21"]/div/div/div[7]/div[2]/div'
-ANCHOR = '//*[@id="app"]/div/div/main/div/div/div[3]/div[1]/table/tbody[1]/tr[377]/td[1]/span' # note that the value can be changed eventually
-INDICES = r'https:\/\/apicr.minzdrav.gov.ru\/api.ashx\?op=GetClinrecPdf&amp;id=(\d{1,4}_\d{1,4})'
-URL = r'https:\/\/apicr.minzdrav.gov.ru\/api.ashx\?op=GetClinrecPdf&amp;id=\d{1,4}_\d{1,4}'
-TITLES = r'https:\/\/apicr.minzdrav.gov.ru\/api.ashx\?op=GetClinrecPdf&amp;id=\d{1,4}_\d{1,4}"\sclass="">(.*?)<\/a><\/td><td>'
+START = Paths().get_start_path()
+END = Paths().get_end_path()
+ANCHOR = Paths().get_anchor()
+INDICES = Regulars().get_indices()
+URL = Regulars().get_url()
+TITLES = Regulars().get_titles()
+
+def start_driver():
+    
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=Service(), options=options)
+
+    return driver
 
 class Page():
-
-    """The class contains functions to find an element on the page to parse the page afterwards."""
 
     def __init__(self):
         pass
 
-    def find_element(self, point: str) -> None:
-
-        """
-        The function accepts an element of a website to click, e.g. a button or a drop-down menu. 
-        Such an element is found by its XPath. Whether there are any errors, an exception will be raised.
-        """
-
+    def find_element(self, driver, point: str) -> None:
         try:
             driver.find_element(By.XPATH, point).click()
         except:
@@ -42,25 +44,15 @@ class Page():
             sys.exit(1)
 
     def parse_page(self, regex: str, html: str) -> list:
-
-        """
-        The function accepts a raw string to parse and returns a list of parsed elements. 
-        Such a string can contain a regular expression as well.
-        """
-
         items = re.findall(regex, html)
         return items
 
 class Title():
-
-    """The class contains functions to change the names of the articles to download them under new names."""
-
+    
     def __init__(self):
         pass
 
     def replace_character(self, titles: list) -> list:
-
-        """The function accepts a list of article titles to remove invalid characters."""
 
         titles_revised = [    
             title.replace(':', '_').replace('/', '_').replace('<', '_').replace('>', '_') 
@@ -70,8 +62,6 @@ class Title():
         return titles_revised
 
     def add_character(self, ids: list, links: list) -> tuple:
-
-        """The function accepts a list of IDs and a list of links to change and replace a character."""
 
         indices_revised = []
         url_revised = []
@@ -87,50 +77,34 @@ class Title():
         return indices_revised, url_revised
 
 def get_urls_list(html: str) -> tuple:
+    
+    title = Title()
+    page = Page()
 
-    '''This HTML page contains the URLs of the articles and some indices used as their names.'''    
     indices = page.parse_page(INDICES, html)
     url = page.parse_page(URL, html)
-    
-    '''We must replace some symbols in the articles titles themselves.'''
+     
     titles_revised = title.replace_character(page.parse_page(TITLES, html))
 
-    '''And change the indices of the articles and their URLs due to the files naming restrictions on Windows 10.'''
     indices_revised, url_revised = title.add_character(indices, url)    
 
-    '''We must be sure that we are going to download the same amount of articles as the number of indices and titles.'''
     assert len(url_revised) == len(indices_revised) == len(titles_revised)
 
-    '''Finally, we have a list of article names to use.'''
-    results = [prefix + title for prefix, title in zip(indices_revised, titles_revised)]
+    results = [prefix + title for prefix, title in zip(indices_revised,titles_revised)]
 
     return results, url_revised
 
-if __name__ == "__main__":
+def main(driver):
 
-    '''Here we're creating all used classes instances to operate them afterwards.'''
-    title = Title()
-    options = Options()
-    page = Page()    
-    service = Service()
-
-    '''We are using the '--headless' option not to open the browser window.'''
-    options.add_argument('--headless')
-
-    '''We are calling the browser with the option in hand and its driver as well.'''
-    driver = webdriver.Chrome(service=service, options=options)
     driver.get(LINK)
+    page = Page()
 
-    '''We must be sure of the website we are going to parse.'''
     TITLE = "Архив клинических рекомендаций"
     assert TITLE in driver.title
 
-    '''Here we are opening the drop-down menu of the articles list.
-    And choosing the 'Все' value to see a full list of articles.'''
-    page.find_element(START)
-    page.find_element(END)
+    page.find_element(driver, START)
+    page.find_element(driver, END)
 
-    '''Here we are waiting just in case the page is not loaded completely.'''
     try:
         wait = WebDriverWait(driver, 1000)
         e = wait.until(expected_conditions.visibility_of_element_located((By.XPATH, ANCHOR)))
@@ -139,11 +113,9 @@ if __name__ == "__main__":
         driver.quit()
         sys.exit(1)
 
-    '''We must get a raw HTML page first.'''
     html_text = driver.page_source
     results, urls = get_urls_list(html_text)
-    
-    '''The rest is to send a GET request to each URL to save an article under a title from the list.'''
+
     for url, result in zip(urls, results):
         try: 
             r = requests.get(url)
@@ -154,3 +126,6 @@ if __name__ == "__main__":
                 print(f"Downloaded: {result}.pdf")
         except Exception as e:
             print(f"Failed to download {result}: {str(e)}")
+
+if __name__ == "__main__":
+    main(start_driver())
